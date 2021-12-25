2ba82678a061458b76f6d083aa1245356c589869
alexthvest <alexthvest@gmail.com>
2021-12-25 12:53:11 +0300

    Confugure theme font and colors

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -11,10 +11,13 @@ export const App: React.FC = () => {
 
   return (
     <div className="container mx-auto my-16">
-      <h1 className="text-2xl font-medium text-fuchsia-700 ">{title}</h1>
-      <p className="text-md font-medium text-gray-400">
+      <h1 className="text-2xl font-medium text-black">{title}</h1>
+      <p className="text-md font-medium text-gray-900">
         This is the students testing system
       </p>
+      <button className="mt-4 text-white font-medium bg-green-300 rounded-md px-8 py-2 transition hover:bg-green-500 active:bg-green-700">
+        Button
+      </button>
     </div>
   );
 };
